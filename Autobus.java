ecba8bf3f51ae8da79ce70348473d10ae03c8461
Yoann Da Silva <yoann-dasilva@orange.fr>
2023-03-15 10:15:47 +0100

    commentaires

--- Autobus.java
+++ Autobus.java
@@ -49,9 +49,9 @@ class Autobus {
     numeroArret++;
     // Create a temporary list to avoid ConcurrentModificationException
     // It has a performance cost, but it's the only way to avoid it
-    ArrayList<PassagerStandard> passagersTmp = new ArrayList<PassagerStandard>(this.passagers);
-    passagersTmp.iterator().forEachRemaining(p -> p.nouvelArret(this, numeroArret));
-    passagersTmp.clear();
+    ArrayList<PassagerStandard> passagersTmp = new ArrayList<PassagerStandard>(this.passagers); // la list copié a les mêmes références (adresses <=> pointeurs) que passagers pointants vers les mêmes objets
+    passagersTmp.iterator().forEachRemaining(p -> p.nouvelArret(this, numeroArret)); 
+    passagersTmp.clear(); // clear the temporary list, garbage collector will do the rest
   }
 
   public void arretDemanderAssis(PassagerStandard p) {
