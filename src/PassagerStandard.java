641a9689a4c88a072c5819593ccc7901406201c6
enaudy <enaudy@enseirb-matemeca.fr>
2023-03-22 12:11:09 +0100

    anxieux

--- src/PassagerStandard.java
+++ src/PassagerStandard.java
@@ -8,12 +8,18 @@ public class PassagerStandard {
   private String nom;
   private int destination;
   private Position position;
+  private int type;
+
+  private int ANXIEUX = 0;
+  private int STANDARD = 1;
 
   // constructor
-  public PassagerStandard(String nom, int destination) {
+  public PassagerStandard(String nom, int destination, int type) {
     this.nom = nom; // this.nom désigne zone mémoire du nom de l'instance
     this.destination = destination;
-    this.position = Position.create(); 
+    this.position = Position.create();
+    this.type = type;
+
   }
 
   //methods
@@ -46,16 +52,29 @@ public class PassagerStandard {
   }
 
   public void monterDans(Autobus t) {
-    if (t.aPlaceAssise()){
-      t.monteeDemanderAssis(this); // 'this' is a PassagerStandard
-    } else if (t.aPlaceDebout()){
-      t.monteeDemanderDebout(this);
+    if (this.type == ANXIEUX) {
+      if (t.aPlaceAssise()){
+      } else if (t.aPlaceDebout()){
+        t.monteeDemanderDebout(this);
+      }
+    } else {
+      if (t.aPlaceAssise()){
+        t.monteeDemanderAssis(this); // 'this' is a PassagerStandard
+      } else if (t.aPlaceDebout()){
+        t.monteeDemanderDebout(this);
+      }
     }
   }
 
   public void nouvelArret(Autobus t, int numeroArret) {
-    if (numeroArret == this.destination) {
-      t.arretDemanderSortie(this);
+    if (this.type == ANXIEUX) {
+      if (numeroArret+1 == this.destination) {
+        t.arretDemanderSortie(this);
+      }
+    } else {
+      if (numeroArret == this.destination) {
+        t.arretDemanderSortie(this);
+      }
     }
   }
 
