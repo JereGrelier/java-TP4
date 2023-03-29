0e3ac8b8e93826ccc01d44ee7d127c13b628b8d3
Jérémy Grelier <jeremy.grelier33@gmail.com>
2023-03-29 08:23:53 +0200

    Edit PassagerStandard to use Passager Interface

--- src/PassagerStandard.java
+++ src/PassagerStandard.java
@@ -3,30 +3,17 @@
 package tec;
 //source without documentation for javadoc
 
-public class PassagerStandard {
+public class PassagerStandard implements Passager{
 
   private String nom;
   private int destination;
   private Position position;
-  private int type;
-
-  private int ANXIEUX = 0;
-  private int STANDARD = 1;
 
   // constructor
   public PassagerStandard(String nom, int destination) {
     this.nom = nom; // this.nom désigne zone mémoire du nom de l'instance
     this.destination = destination;
     this.position = Position.create();
-    this.type = 1;
-
-  }
-
-  public PassagerStandard(String nom, int destination, int type) {
-    this.nom = nom; // this.nom désigne zone mémoire du nom de l'instance
-    this.destination = destination;
-    this.position = Position.create();
-    this.type = type;
 
   }
 
@@ -60,36 +47,17 @@ public class PassagerStandard {
   }
 
   public void monterDans(Autobus t) {
-    if (this.type == ANXIEUX) {
-      monterCaractereAnxieux(t);
-    } else {
-      monterCaractereStandard(t);
-    }
-  }
-
-  private void monterCaractereStandard(Autobus t) {
     if (t.aPlaceAssise()){
       t.monteeDemanderAssis(this); // 'this' is a PassagerStandard
     } else if (t.aPlaceDebout()){
       t.monteeDemanderDebout(this);
     }
   }
-  private void monterCaractereAnxieux (Autobus t) {
-    if (t.aPlaceDebout()){
-      t.monteeDemanderDebout(this);
-    }
-  }
 
   public void nouvelArret(Autobus t, int numeroArret) {
-    if (this.type == ANXIEUX) {
-      if (numeroArret+1 == this.destination) {
-        t.arretDemanderSortie(this);
-      }
-    } else {
       if (numeroArret == this.destination) {
         t.arretDemanderSortie(this);
       }
-    }
   }
 
   @Override // surcharge de la méthode toString() de la classe Object
