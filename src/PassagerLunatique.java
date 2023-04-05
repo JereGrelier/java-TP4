ee766e6b9ee8ca90926383bef01b6e91d9b766e5
Jérémy Grelier <jeremy.grelier33@gmail.com>
2023-04-05 11:50:44 +0200

    even more fix

--- src/PassagerLunatique.java
+++ src/PassagerLunatique.java
@@ -2,11 +2,8 @@ package tec;
 
 public class PassagerLunatique extends PassagerStandard {
 
-    private int destination;
-
     public PassagerLunatique(String nom, int destination) {
         super(nom, destination); // call to super class constructor
-        this.destination = destination;
     }
     // A la montée, même code que PassagerStandard
 
