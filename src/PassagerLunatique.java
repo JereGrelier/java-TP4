353bca5fbbaca904c97a6475d58b86909e5cbce1
Yoann Da Silva <yoann-dasilva@orange.fr>
2023-04-05 11:54:18 +0200

    Correction avec aide prof

--- src/PassagerLunatique.java
+++ src/PassagerLunatique.java
@@ -2,18 +2,14 @@ package tec;
 
 public class PassagerLunatique extends PassagerStandard {
 
-    private int destination;
-
     public PassagerLunatique(String nom, int destination) {
         super(nom, destination); // call to super class constructor
-        this.destination = destination;
     }
     // A la montée, même code que PassagerStandard
 
     public void nouvelArret(Autobus t, int numeroArret) {
-        if (numeroArret >= this.destination) { // >= sinon on change de place alors qu'on est dehors
-            t.arretDemanderSortie(this);
-        } else if (this.estAssis()) {
+        super.nouvelArret(t, numeroArret);
+        if (this.estAssis()) {
             t.arretDemanderDebout(this);
         } else if (this.estDebout()) {
             t.arretDemanderAssis(this);
