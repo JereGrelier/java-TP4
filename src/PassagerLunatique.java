6ea86b9a7b9b8679cbfb29adffefcc0e3833c612
Jérémy Grelier <jeremy.grelier33@gmail.com>
2023-04-05 10:47:25 +0200

    PassagerLunatique

--- src/PassagerLunatique.java
+++ src/PassagerLunatique.java
@@ -2,8 +2,23 @@ package tec;
 
 class PassagerLunatique extends PassagerStandard {
 
+    int destination;
+    String name;
+
     PassagerLunatique(String nom, int destination) {
         super(nom, destination);
+    }
+
+    public void nouvelArret(Autobus t, int numeroArret) {
+        if (numeroArret == this.destination) {
+          t.arretDemanderSortie(this);
+        }
+        else if(this.estAssis()) {
+            t.arretDemanderDebout(this);
+        }
+        else if(this.estDebout()) {
+            t.arretDemanderAssis(this);
+        }
     }
 
 }
