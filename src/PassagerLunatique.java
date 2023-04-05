885829de4df171cde35e1b1c726aa62cda5e7521
Yoann Da Silva <yoann-dasilva@orange.fr>
2023-04-05 11:21:25 +0200

    commentaires

--- src/PassagerLunatique.java
+++ src/PassagerLunatique.java
@@ -6,17 +6,16 @@ public class PassagerLunatique extends PassagerStandard {
     String name;
 
     public PassagerLunatique(String nom, int destination) {
-        super(nom, destination);
+        super(nom, destination); // call to super class constructor
     }
-
+    // A la montée, même code que PassagerStandard
+    
     public void nouvelArret(Autobus t, int numeroArret) {
         if (numeroArret == this.destination) {
-          t.arretDemanderSortie(this);
-        }
-        else if(this.estAssis()) {
+            t.arretDemanderSortie(this);
+        } else if (this.estAssis()) {
             t.arretDemanderDebout(this);
-        }
-        else if(this.estDebout()) {
+        } else if (this.estDebout()) {
             t.arretDemanderAssis(this);
         }
     }
