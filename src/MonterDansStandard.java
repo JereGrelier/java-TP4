33cc49dbe95e5262dbca368f6117c8f302fdeb30
enaudy <enaudy@enseirb-matemeca.fr>
2023-04-12 13:57:54 +0200

    MoneterDansStandard

---
+++ src/MonterDansStandard.java
@@ -0,0 +1,12 @@
+public abstract class MonterDansStandard extends PassagerAbstrait{
+    public MonterDansStandard(String nom, int destination) {
+        super(nom, destination);
+    }
+    public void monterDans(Autobus t) {
+        if (t.aPlaceAssise()) {
+            t.monteeDemanderAssis(this);
+        } else if (t.aPlaceDebout()) {
+            t.monteeDemanderDebout(this);
+        }
+    }
+}
