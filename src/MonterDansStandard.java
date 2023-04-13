b64d7ff5a94a2235b241507848d1585683633466
Jérémy Grelier <jeremy.grelier33@gmail.com>
2023-04-13 20:35:07 +0200

    tptp

--- src/MonterDansStandard.java
+++ src/MonterDansStandard.java
@@ -1,12 +1,15 @@
 package tec;
 
-public abstract class MonterDansStandard extends PassagerAbstrait{
+public abstract class MonterDansStandard extends PassagerAbstrait {
     public MonterDansStandard(String nom, int destination) {
         super(nom, destination);
     }
+
     public void monterDans(Autobus t) {
         if (t.aPlaceAssise()) {
             t.monteeDemanderAssis(this);
         } else if (t.aPlaceDebout()) {
+            t.monteeDemanderDebout(this);
+        }
     }
 }
